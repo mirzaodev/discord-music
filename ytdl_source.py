0c60fe4b799d89f0b6249f05dcf07572613b0378
Mirza Omercic <mirza.os632@gmail.com>
2026-02-24 14:04:12 +0100

    again :(

--- ytdl_source.py
+++ ytdl_source.py
@@ -4,7 +4,7 @@ import discord
 import yt_dlp
 
 _YTDL_OPTIONS = {
-    "format": "bestaudio/best",
+    "format": "bestaudio[ext=m4a]/bestaudio[ext=webm]/bestaudio*/best",
     "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
     "restrictfilenames": True,
     "noplaylist": True,
